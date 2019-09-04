494a573be2ea826752096d614ec64e9bf75e1c31
rummolprod999 <rummolprod999@gmail.com>
2019-09-04 11:01:19 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/cpp_mysql.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_mysql.dir/main.cpp.o"
+  "cpp_mysql.pdb"
+  "cpp_mysql"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_mysql.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
