aa4cc692a38ca0e6fa45e68cee660c213caecc71
rummolprod999 <rummolprod999@gmail.com>
2019-09-04 11:02:03 +0300

    upd

--- main.cpp
+++ main.cpp
@@ -1,6 +1,72 @@
+//https://dev.mysql.com/doc/connector-cpp/1.1/en/connector-cpp-examples-complete-example-2.html
+#include <stdlib.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include "mysql_connection.h"
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
+
+using namespace std;
+
+int main(void)
+{
+    cout << endl;
+    cout << "Let's have MySQL count from 10 to 1..." << endl;
+
+    try {
+        sql::Driver *driver;
+        sql::Connection *con;
+        sql::Statement *stmt;
+        sql::ResultSet *res;
+        sql::PreparedStatement *pstmt;
+
+        /* Create a connection */
+        driver = get_driver_instance();
+        con = driver->connect("tcp://localhost:3306", "root", "1234");
+        /* Connect to the MySQL test database */
+        con->setSchema("test");
+
+        stmt = con->createStatement();
+        stmt->execute("DROP TABLE IF EXISTS test");
+        stmt->execute("CREATE TABLE test(id INT)");
+        delete stmt;
+
+        /* '?' is the supported placeholder syntax */
+        pstmt = con->prepareStatement("INSERT INTO test(id) VALUES (?)");
+        for (int i = 1; i <= 10; i++) {
+            pstmt->setInt(1, i);
+            pstmt->executeUpdate();
+        }
+        delete pstmt;
+
+        /* Select in ascending order */
+        pstmt = con->prepareStatement("SELECT id FROM test ORDER BY id ASC");
+        res = pstmt->executeQuery();
+
+        /* Fetch in reverse = descending order! */
+        res->afterLast();
+        while (res->previous())
+            cout << "\t... MySQL counts: " << res->getInt("id") << endl;
+        delete res;
+
+        delete pstmt;
+        delete con;
+
+    } catch (sql::SQLException &e) {
+        cout << "# ERR: SQLException in " << __FILE__;
+        cout << "(" << __FUNCTION__ << ") on line "
+        << __LINE__ << endl;
+        cout << "# ERR: " << e.what();
+        cout << " (MySQL error code: " << e.getErrorCode();
+        cout << ", SQLState: " << e.getSQLState() <<
+        " )" << endl;
+    }
+
+    cout << endl;
+
+    return EXIT_SUCCESS;
 }
